a14cad9d22d74f87244b7bf86f9b02bb8ea88b24
Lechien62 <totorieder1@gmail.com>
2017-03-28 19:46:04 +0200

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,91 @@
+/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.04
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead*/
+var l_serverImage = "images/logo.png";
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "ThecArch";
+
+/*Use a video for the background?
+Otherwise it will just use images*/
+var l_bgVideo = false;
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them*/
+var l_bgImages = [
+	"backgrounds/images/touhou01.jpg",
+	"backgrounds/images/touhou02.jpg",
+	"backgrounds/images/touhou03.jpg"
+];
+
+/*Random background images order?*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Music player
+'true' or 'false' to enable music in the background*/
+var l_music = true;
+
+/*Display actual song's name?*/
+var l_musicDisplay = true;
+
+/*Music playlist
+Place how much Youtube IDs/.ogg you want*/
+var l_musicPlaylist = [
+	{ogg: "songs/song1.ogg", name: "TheKairi78 - Vos mères les tricératops"},
+    {ogg: "songs/song2.ogg", name: "Mozzik - Cocaina"},
+	{ogg: "songs/song3.ogg", name: "Ariana Grande - Everyday"}
+];
+
+/*Random music order?*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 and 100*/
+var l_musicVolume = 15;
+
+/*Enable custom messages?*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+"Bienvenue sur ThecArchRP",
+"Bon jeux à tous !"
+];
+
+/*Random message order?*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
